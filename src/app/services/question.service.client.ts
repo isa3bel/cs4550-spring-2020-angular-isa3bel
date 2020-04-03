7aa44896cdd929cd12a38c31f2a3b7cae6b261b2
Isabel B <isabelbolger@hotmail.com>
2020-04-02 19:56:59 -0700

    changed url

--- src/app/services/question.service.client.ts
+++ src/app/services/question.service.client.ts
@@ -2,6 +2,6 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuestionServiceClient {
   findQuestionsForQuiz = (qid) =>
-    fetch(`http://localhost:3000/api/quizzes/${qid}/questions`)
+    fetch(`https://cs4550-isabelb-node-hw8.herokuapp.com/api/quizzes/${qid}/questions`)
       .then(response => response.json())
 }
--- src/app/services/quiz.service.client.ts
+++ src/app/services/quiz.service.client.ts
@@ -2,9 +2,9 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuizServiceClient {
   findAllQuizzes = () =>
-    fetch('http://localhost:3000/api/quizzes')
+    fetch('https://cs4550-isabelb-node-hw8.herokuapp.com/api/quizzes')
       .then(response => response.json())
   findQuizById = (qid) =>
-    fetch(`http://localhost:3000/api/quizzes/${qid}`)
+    fetch(`https://cs4550-isabelb-node-hw8.herokuapp.com/api/quizzes/${qid}`)
       .then(response => response.json())
 }
