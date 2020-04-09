b8143bfa276828455c23268e8c4fd13eadb6f719
Isabel B <isabelbolger@hotmail.com>
2020-04-09 11:43:52 -0700

    changed multiple choice questions for hw 9

--- src/app/components/multiple-choice-question/multiple-choice-question.component.ts
+++ src/app/components/multiple-choice-question/multiple-choice-question.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-multiple-choice-question',
@@ -11,18 +11,29 @@ export class MultipleChoiceQuestionComponent implements OnInit {
 
   @Input()
   question
+  @Input()
   answer = null
   grading = false
   correct
-  grade = () => {
+  @Output()
+  answerChange = new EventEmitter<string>()
+  // submitAnswer = () => {
+  //   if (this.answer === null ) {
+  //     return;
+  //   }
+  //   this.answerChange.emit(this.answer)
+  // }
+ 
+  submitAnswer = () => {
     if (this.answer === null ) {
-      return false;
+      return;
     }
     this.grading = true;
     console.log(this.grading);
     if (this.answer === this.question.correct) {
       this.correct = true;
     }
+    this.answerChange.emit(this.answer)
   };
 
   ngOnInit(): void {
