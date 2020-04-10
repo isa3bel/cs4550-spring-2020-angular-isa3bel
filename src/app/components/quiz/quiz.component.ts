bbafeb72df18be9d1f5933b596e347a94b037f20
Isabel B <isabelbolger@hotmail.com>
2020-04-10 14:42:09 -0700

    changed urls

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,38 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import { QuestionServiceClient } from 'src/app/services/question.service.client';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { QuestionServiceClient } from "src/app/services/question.service.client";
+import { QuizServiceClient } from "src/app/services/quiz.service.client";
 
 @Component({
-  selector: 'app-quiz',
-  templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.css']
+  selector: "app-quiz",
+  templateUrl: "./quiz.component.html",
+  styleUrls: ["./quiz.component.css"],
 })
 export class QuizComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private service: QuestionServiceClient,
+    private service2: QuizServiceClient
+  ) {}
 
-  constructor(private route: ActivatedRoute, private service: QuestionServiceClient) { }
-
-  questions = []
-  quizId = ''
+  questions = [];
+  quizId = "";
+  attempt;
+  prevAttempts: Int32Array[] = [];
 
   submitQuiz = () => {
-    fetch(`http://localhost:3000/api/quizzes/${this.quizId}/attempts`, {
-      method: 'POST',
+    fetch(`https://cs4550-isabel-b-hw9-node.herokuapp.com/api/quizzes/${this.quizId}/attempts`, {
+      method: "POST",
       body: JSON.stringify(this.questions),
       headers: {
-        'content-type': 'application/json'
-      }
-    }).then(response => response.json())
-      .then(result => console.log(result))
-   }
-
-   
-  ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.quizId = params.quizId;
-      this.service.findQuestionsForQuiz(this.quizId)
-      .then(questions => this.questions = questions);  
+        "content-type": "application/json",
+      },
     })
-   
-  }
+      .then((response) => response.json())
+      .then((result) => {
+        this.attempt = result["score"];
+      });
+      this.getAllAttempts()
+  };
 
+  getAllAttempts = () => {
+    this.prevAttempts = [];
+    return this.service2.findAttemptsForQuiz(this.quizId).then((response) => {
+      
+      for (var i = 0; i < response.length; i++) {
+        this.prevAttempts.push(response[i].score);
+      }
+    });
+    
+  };
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.quizId = params.quizId;
+      this.service
+        .findQuestionsForQuiz(this.quizId)
+        .then((questions) => (this.questions = questions));
+    });
+    this.getAllAttempts()
+  }
 }
--- src/app/services/quiz.service.client.ts
+++ src/app/services/quiz.service.client.ts
@@ -1,10 +1,16 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from "@angular/core";
 @Injectable()
 export class QuizServiceClient {
   findAllQuizzes = () =>
-    fetch('http://localhost:3000/api/quizzes')
-      .then(response => response.json())
+    fetch("https://cs4550-isabel-b-hw9-node.herokuapp.com/api/quizzes").then((response) =>
+      response.json()
+    );
   findQuizById = (qid) =>
-    fetch(`http://localhost:3000/api/quizzes/${qid}`)
-      .then(response => response.json())
+    fetch(`https://cs4550-isabel-b-hw9-node.herokuapp.com/api/quizzes/${qid}`).then((response) =>
+      response.json()
+    );
+  findAttemptsForQuiz = (qid) =>
+    fetch(
+      `https://cs4550-isabel-b-hw9-node.herokuapp.com/api/quizzes/${qid}/attempts`
+    ).then((response) => response.json());
 }
